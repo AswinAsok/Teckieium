dd474b68ed5048407b14ef563f7cd2b2e53c4c6d
Aswin Asok <aswinasokofficial@gmail.com>
2021-01-07 15:52:24 +0530

    Blogs added to admin panel

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import BlogPost
 
 # Register your models here.
+
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('blog_title','blog_author', 'blog_date')
+    search_fields = ('blog_title', 'blog_author')
+
+    class Meta:
+        model = BlogPost
+
+admin.site.register(BlogPost,BlogPostAdmin)
--- Blog/models.py
+++ Blog/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class BlogPost(models.Model):
     blog_title = models.CharField(max_length = 30, blank=False)
-    post_date = models.DateTimeField(blank=False),
-    blog_content = models.CharField(blank=False),
+    blog_author = models.CharField(max_length=30, blank=False,default="")
+    blog_content = models.TextField(max_length=5000, blank=False,default="")
+    blog_date = models.DateTimeField(blank=True, null=True) 
 
     def __str__(self):
         return self.blog_title
+
+
+
 
