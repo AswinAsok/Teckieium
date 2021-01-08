71fb5cb34ea78ceba0080293e9635a29367181e0
Aswin Asok <aswinasokofficial@gmail.com>
2021-01-08 17:51:51 +0530

    Some minor changes

--- Blog/models.py
+++ Blog/models.py
@@ -8,7 +8,6 @@ class BlogPost(models.Model):
     blog_author = models.CharField(max_length=30, blank=False,default="")
     blog_content = models.TextField(max_length=5000, blank=False,default="")
     blog_date = models.DateField(blank=True, null=True) 
-    blog_time = models.TimeField(blank=True, null=True) 
 
     def __str__(self):
         return self.blog_title
