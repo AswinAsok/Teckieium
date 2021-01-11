9560085d9b5072a6bee64a630faf47927990d868
Aswin Asok <aswinasokofficial@gmail.com>
2021-01-11 19:20:06 +0530

    Some files added

--- Teckieium/settings.py
+++ Teckieium/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '*=*k1688_wn+s!ycfj7gzt!pw73t=5_3a06*u=p!@$ak(v$!#$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['teckieium.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
